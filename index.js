14db2dac667bf47cd24876c52a8fe46e43af77c5
Sagar Jadhav <93977940+sagarxjadhav@users.noreply.github.com>
2024-01-11 01:39:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const taskInput = document.querySelector('#task-input');
+const addBtn = document.querySelector('#add-btn');
+const taskList = document.querySelector('#task-list');
+
+// Event Listeners
+
+addBtn.addEventListener('click',()=>{
+    if(taskInput.value!== ""){
+        const taskText = taskInput.value;
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `${taskText} <button class="delete-btn">Delete</button>`;
+        taskList.appendChild(listItem);
+        taskInput.value = "";
+    }
+});
+
+taskList.addEventListener('click',(event)=>{
+    if(event.target.classList.contains("delete-btn")){
+        const listItem = event.target.parentElement;
+        taskList.removeChild(listItem);
+    }
+})
